4dcba5eebc3272e69e8aac238781c21be57301b7
benjamin <benjamin.roullet@gmail.com>
2020-10-09 18:50:45 +0200

    :construction: Try use IOS to read RFID

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,9 +1,10 @@
+import { Platform } from '@ionic/angular';
 import { Component, OnInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 
 import { TranslationService } from '../services/translation.service';
 
-import { NFC, NfcUtil } from '@ionic-native/nfc/ngx';
+import { NFC, NfcUtil, NfcTag } from '@ionic-native/nfc/ngx';
 import { Geolocation } from '@ionic-native/geolocation/ngx';
 import { Router } from '@angular/router';
 
@@ -27,6 +28,7 @@ export class HomePage implements OnInit {
     private goelocation: Geolocation,
     private translationService: TranslationService,
     private router: Router,
+    private platform: Platform,
   ) {
     this.translationService.initLanguage();
   }
@@ -36,15 +38,29 @@ export class HomePage implements OnInit {
 
   startScan(): void {
     this.homeStep = 'waiting';
-    this.nfc.readerMode(this.nfc.FLAG_READER_NFC_V).subscribe(
-      async (nfcEvent) => {
-        console.log('reader mode success');
-        this.router.navigate(['scan']);
-      }, (error) => {
-        console.log('Error readerMode', error);
-        this.homeStep = 'error';
-      }
-    );
+    if (this.platform.is('ios')) {
+      console.log('Start scanning IOS');
+      this.nfc.scanTag().then(
+        (tag: NfcTag) => {
+          console.log('success scan IOS rfid');
+          console.log(tag);
+          console.log(tag.ndefMessage);
+        },
+        (error) => {
+          console.log(error);
+        }
+      )
+    } else {
+      this.nfc.readerMode(this.nfc.FLAG_READER_NFC_V).subscribe(
+        async (nfcEvent) => {
+          console.log('reader mode success');
+          this.router.navigate(['scan']);
+        }, (error) => {
+          console.log('Error readerMode', error);
+          this.homeStep = 'error';
+        }
+      );
+    }
   }
 
   newScan(): void {
